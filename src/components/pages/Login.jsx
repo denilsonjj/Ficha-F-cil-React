43d785432b4733a84ea0a5ed97eab72adcf0e55b
Denilson junior <juniordenilson363@gmail.com>
2022-04-21 22:25:28 -0300

    adicionando react-icons e criando form de cadastro

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import NavBar from "../Nav/Navbar";
 import style from "./Login.module.css";
+import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { RiLockPasswordLine } from "react-icons/ri";
+
 const Login = () => {
   return (
     <>
@@ -14,7 +18,7 @@ const Login = () => {
           <h3 className={style.principal}>Bem-vindo de volta!</h3>
           <p>Acesse sua Conta agora.</p>
           <input type={"button"} value={"ENTRAR"} className={style.button} />
-          <p style={{ cursor: "pointer" }}>Esqueceu sua senha?</p>
+          <p className={style.forget}>Esqueceu sua senha?</p>
         </div>
         <div className={style.container2}>
           <h3 className={style.principal2}>
@@ -24,8 +28,31 @@ const Login = () => {
           <p style={{ color: "gray" }}>Preencha seus dados</p>
           <form style={{ display: "flex", flexDirection: "column" }}>
             <div>
-          
-              <input className={style.input_form} type="text" placeholder="Nome" />
+              <>
+                <AiOutlineUser className={style.icons} />
+                <input
+                  className={style.input_form}
+                  type="text"
+                  placeholder="Nome"
+                />
+              </>
+              <div>
+                <AiOutlineMail className={style.icons} />
+                <input
+                  className={style.input_form}
+                  type="email"
+                  placeholder="Email"
+                />
+              </div>
+              <div>
+                <RiLockPasswordLine className={style.icons} />
+                <input
+                  className={style.input_form}
+                  type="password"
+                  placeholder="Senha"
+                />
+              </div>
+              <input type="submit" value="Enviar" />
             </div>
           </form>
         </div>
